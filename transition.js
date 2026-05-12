25c9dd0bc9627c39577e428891e49118be317b25
Nirvana-Sys199 <jarredkai.cokimso.cics@ust.edu.ph>
2026-05-12 09:54:23 +0800

    Update transition.js

--- transition.js
+++ transition.js
@@ -1,15 +1,15 @@
 
-  // "Book" button in navbar
+
   const bookButton = document.querySelector('a[href="book.html"]');
   const curtain = document.querySelector('.page-transition');
 
-  if(bookButton) { // Check if the button exists on this page
+  if(bookButton) {
     bookButton.addEventListener('click', function(e) {
-      e.preventDefault(); // Stop the browser from leaving immediately
+      e.preventDefault();
       
-      curtain.classList.add('active'); // Slide curtain 
+      curtain.classList.add('active');
       
-      // Wait 1.5 seconds, then actually go to the booking page
+
       setTimeout(() => {
         window.location.href = "book.html";
       }, 1000);
